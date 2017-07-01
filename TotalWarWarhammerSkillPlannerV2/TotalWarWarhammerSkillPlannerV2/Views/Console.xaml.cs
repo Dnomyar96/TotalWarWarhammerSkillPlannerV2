a0e67498fe8eb3c021814c5fd46893a9d18d5bd8
Raymond Benjamins <raymond.benjamins@telfort.nl>
2017-07-01 16:35:06 +0200

    Added a console for logging and debugging purposes. This has to be removed for release versions!

---
+++ TotalWarWarhammerSkillPlannerV2/TotalWarWarhammerSkillPlannerV2/Views/Console.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TotalWarWarhammerSkillPlannerV2.Views
+{
+    /// <summary>
+    /// Interaction logic for Console.xaml
+    /// </summary>
+    public partial class Console : Window
+    {
+        private string log;
+        private string text;
+        private bool logShown;
+
+        public Console()
+        {
+            InitializeComponent();
+            logShown = true;
+        }
+
+        public void WriteLine(string txt = "")
+        {
+            if (!logShown)
+            {
+                textBlock.Text += txt + Environment.NewLine;
+                scroller.ScrollToEnd();
+            }
+            else
+            {
+                text += txt + Environment.NewLine;
+            }
+        }
+
+        public void Log(string lg = "")
+        {
+            if (logShown)
+            {
+                textBlock.Text += lg + Environment.NewLine;
+                scroller.ScrollToEnd();
+            }
+            else
+            {
+                log += lg + Environment.NewLine;
+            }
+        }
+
+        public void Clear()
+        {
+            textBlock.Text = "";
+        }
+
+        public void ShowLog()
+        {
+            if(!logShown)
+            {
+                text = textBlock.Text;
+            }
+            else
+            {
+                log = textBlock.Text;
+            }
+
+            textBlock.Text = log;
+            logShown = true;
+        }
+
+        public void ShowText()
+        {
+            if (!logShown)
+            {
+                text = textBlock.Text;
+            }
+            else
+            {
+                log = textBlock.Text;
+            }
+
+            textBlock.Text = text;
+            logShown = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ShowLog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ShowText();
+        }
+    }
+}
--- TotalWarWarhammerSkillPlannerV2/TotalWarWarhammerSkillPlannerV2/Views/MainWindow.xaml.cs
+++ TotalWarWarhammerSkillPlannerV2/TotalWarWarhammerSkillPlannerV2/Views/MainWindow.xaml.cs
@@ -24,55 +24,73 @@ namespace TotalWarWarhammerSkillPlannerV2.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Console console;
+
         public MainWindow()
         {
             InitializeComponent();
             DataHandler.SetBasePath(@"E:\Steam\steamapps\common\Total War WARHAMMER\assembly_kit\raw_data\db\");
+
+            console = new Console();
+            console.Show();
+            console.WriteLine("Started");
+            console.Log("Started");
         }
 
         private void skillsBtn_Click(object sender, RoutedEventArgs e)
         {
+            console.Log("Skills button clicked");
             gridBox.ItemsSource = DataHandler.GetSkills();
         }
 
         private void nodesBtn_Click(object sender, RoutedEventArgs e)
         {
+            console.Log("Nodes button clicked");
             gridBox.ItemsSource = DataHandler.GetSkillNodes();
         }
 
         private void Links_Click(object sender, RoutedEventArgs e)
         {
+            console.Log("Links button clicked");
             gridBox.ItemsSource = DataHandler.GetSkillNodeLinks();
         }
 
         private void levelDetails_Click(object sender, RoutedEventArgs e)
         {
+            console.Log("LevelDetails button clicked");
             gridBox.ItemsSource = DataHandler.GetSkillLevelDetails();
         }
 
         private void levelToEffect_Click(object sender, RoutedEventArgs e)
         {
+            console.Log("LevelToEffect button clicked");
             gridBox.ItemsSource = DataHandler.GetSkillLevelToEffects();
         }
 
         private void nodeSets_Click(object sender, RoutedEventArgs e)
         {
+            console.Log("NodeSets button clicked");
+            console.WriteLine("Test");
             gridBox.ItemsSource = DataHandler.GetSkillNodeSets();
         }
 
         private void agentSubtypes_Click(object sender, RoutedEventArgs e)
         {
+            console.Log("AgentSubtypes button clicked");
             gridBox.ItemsSource = DataHandler.GetAgentSubTypes();
         }
 
         private void effectsBtn_Click(object sender, RoutedEventArgs e)
         {
+            console.Log("Effects button clicked");
             gridBox.ItemsSource = DataHandler.GetEffects();
         }
 
         private void objectBtn_Click(object sender, RoutedEventArgs e)
         {
-            gridBox.ItemsSource = DataHandler.CreateDataObjects();
+            console.Log("Object button clicked");
+            var data = DataHandler.CreateDataObjects();
+            gridBox.ItemsSource = data;
         }
     }
 }
